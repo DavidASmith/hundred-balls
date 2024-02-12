29e667ee876ed76657d44449f7d19f3a39487f06
David Smith <david.alex.smith@gmail.com>
2024-02-12 16:09:05 +0000

    Added colour theme

--- sketch.js
+++ sketch.js
@@ -1,13 +1,15 @@
 
 // Global variables
 let balls; // All balls already dropped
-// let ball; 
 let balls_left;
 let new_ball, new_ball_diam, adding_ball; // Ball currently being dropped	
 let boundaries, floor, l_wall, r_wall, ceiling; // screen boundaries
 let baddies;
 let inflate_sound, pop_sound, add_ball_sound, level_up_sound;
 let level;
+let screen_area;
+
+let baddie_colour, ball_colour, background_colour, text_colour, progress_colour, adding_ball_colour, target_colour, progress_background_colour;
 
 function preload() {
 	inflate_sound = loadSound('assets/sounds/inflate.wav');
@@ -20,6 +22,15 @@ function setup() {
 	new Canvas(600, 400);
 
 	// Setup world
+	//screen_area = canvas.w * canvas.h;
+	baddie_colour = color(204, 51, 63);
+	ball_colour = color(0, 160, 176);
+	background_colour = color(106, 74, 60);
+	text_colour = color(237, 201, 81);
+	progress_colour = color(0, 160, 176);
+	adding_ball_colour = color(204, 51, 63);
+	target_colour = color(237, 201, 81);
+	progress_background_olour = color(235, 104, 65);
 
 	// Define boundaries for game world
 	boundaries = new Group;
@@ -31,11 +42,13 @@ function setup() {
 
 	// Initialise balls
 	balls = new Group();
+	balls.color = ball_colour;
 	new_ball_diam = 1;
 	balls_left = 100;
 
 	// Initialise baddies
 	baddies = new Group();
+	baddies.color = baddie_colour;
 	baddy = new baddies.Sprite(300, 200, 10, 10, collider = 'd');
 
 	level = 1;
@@ -46,7 +59,7 @@ function draw() {
 	
 	clear();
 	
-	background('gray');
+	background(background_colour);
 	
 
 	// If we're currently adding a ball
@@ -101,6 +114,7 @@ function mouseReleased() {
 function mousePressed() {
 	adding_ball = true;
 	new_ball = new Sprite(mouseX, mouseY, new_ball_diam, collider = 's');
+	new_ball.color = ball_colour;
 	inflate_sound.play();
 }
 
@@ -115,7 +129,7 @@ function addBall() {
 
 
 function displayLives() {
-	fill('red');
+	fill(text_colour);
 	textSize(32);
 	textAlign(RIGHT);
 	text(balls_left, width - 20, height - 20);
@@ -124,7 +138,7 @@ function displayLives() {
   }
 
 function displayLevel() {
-	fill('red');
+	fill(text_colour);
 	textSize(32);
 	textAlign(LEFT);
 	text(level, 20, height - 20);
